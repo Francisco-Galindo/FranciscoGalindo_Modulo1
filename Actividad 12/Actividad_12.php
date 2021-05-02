8282d88dd9abb1e7e51d2b442cdf61afe40e807e
Francisco-Galindo <paqui10718@gmail.com>
2021-05-02 12:27:43 -0500

    REFACTOR: La traducción de morse a español es mucho más simple

--- Actividad 12/Actividad_12.php
+++ Actividad 12/Actividad_12.php
@@ -10,7 +10,6 @@
     <?php
         /* Este programa se encarga de traducir ya sea texto a morse o viceversa, dependiendo de la información recibida en el formulario */
 
-
         // Recibiendo datos del formulario
         $cadena =  $_POST["texto"];
         $action =  $_POST["tipoCifrado"];
@@ -72,35 +71,16 @@
                     }
                 }
 
-                // Iterando por cada letra del texto
-                for ($i = 0; $i < strlen($cadena) && $textoValido; $i++) {
+                // Separando cada una de las letras del texto en morse en un arreglo
+                $cadeaArreglo = explode(" ", $cadena); 
+
+                // Iterando por todas las subcadenas del arreglo, cada una representando una letra en morse
+                foreach ($cadeaArreglo as $indice => $subcadena) {
+
                     // Iterando por cada una de las correspondencias
                     foreach ($correspondenciasMorse as $normal => $morse) {
-                        /* 
-                        Continuar únicamente si hay suficiente espacio como para que quepa la
-                         representación en morse de la correspondencia que se está checando y 
-                        es el inicio de una letra 
-                        */
-                        if ($i + strlen($morse) <= strlen($cadena) && ($i === 0 || $cadena[$i - 1]  === " " )) {
-
-                            /* La subcadena que se utilizará para compararse con la representación en
-                            morse de la letra */
-                            $subcadena = substr($cadena, $i, strlen($morse));
-
-                            /* 
-                            Una subcadena sólo abarcará el espacio correspondiente a una letra ya sea si 
-                            el caracter siguiente a la misma es un espacio o es el final del texto 
-                            */
-                            $subcadenaAbarcaLetra = ($i + strlen($morse) == strlen($cadena) || $cadena[$i + strlen($morse)] === " ");
-
-                            /* 
-                            En caso de que la subcadena sea igual a la representación en morse
-                            de la letra con la que se está comparando y que la subcadena abarque toda la letra, 
-                            se imprime la representación en español de la letra 
-                            */
-                            if ($subcadena == $morse && $subcadenaAbarcaLetra) {
-                                $traduccion .= $normal;
-                            }
+                        if ($subcadena == $morse) {
+                            $traduccion .= $normal;
                         }
                     }
                 }
