cc005b94397ef900766a70da65aed0e1c276e8ed
Francisco-Galindo <paqui10718@gmail.com>
2021-05-03 21:45:12 -0500

    Refactor: Mejorando el algoritmo para checar si se ha conseguido la victoria

--- Actividad 13/index.php
+++ Actividad 13/index.php
@@ -7,13 +7,15 @@
     <title>Juego</title>
 </head>
   <body>
-    <h1>Juego XD</h1>
+    <h1>Juego de barquitos pium pium</h1>
     <?php 
       /* Juego de batalla naval con PHP */
 
       const CASILLA_VACIA = "./water.png";
-      const CASILLA_LLENA = "./xd";
-      const CASILLA_LLENA = "./xd";
+
+      // const CASILLA_LLENA = "./boat.png"; //Descomentar para que se vea dónde están los barcos
+      const CASILLA_LLENA = "./water2.png"; // Descomentar para juego normal
+
       const CASILLA_FALLO = "./barrier.png";
       const CASILLA_GOLPE = "./fire.png";
 
@@ -25,13 +27,13 @@
         
         $tableroDeJuegoTamano = count($tableroDeJuego);
         do {
-          $creadoCorrectamente = true;
+          $creadoCorrectamente = false;
           // Este arreglo permite conocer todas las posiciones del tablero 
           // donde se encuentra el barco en creación
           $barcoPosiciones = [];
 
-          $posX = rand(0, $tableroDeJuegoTamano-$barcoTamano);
-          $posY = rand(0, $tableroDeJuegoTamano-$barcoTamano);
+          $posX = rand(0, $tableroDeJuegoTamano-1);
+          $posY = rand(0, $tableroDeJuegoTamano-1);
 
           // Representa la dirección en ambos ejes del barco, 
           // Si el barco se mueve en el eje x, no debe moverse en el eje y,
@@ -40,7 +42,11 @@
           $deltaY = $deltaX == 0 ? 1 : 0;
 
           for ($i = 0; $i < $barcoTamano; $i++) {
-            $casillaLibre = ($tableroDeJuego[$posX][$posY] == CASILLA_VACIA); 
+            $creadoCorrectamente = true;
+            $casillaLibre = false;
+            if ($posX < $tableroDeJuegoTamano && $posY < $tableroDeJuegoTamano) {
+              $casillaLibre = ($tableroDeJuego[$posX][$posY] == CASILLA_VACIA);
+            } 
             
             // Llenando la casilla con una parte del barco
             if ($casillaLibre) {
@@ -95,103 +101,99 @@
 
       $vidas = isset($_POST["vidas"]) ? $_POST["vidas"] : $vidasMax;
 
-      // Una partida es nueva si la cantidad de vidas es igual a la cantidad
-      // máxima de vidas posibles y no se ha recibido ningún valor de vidas en
-      // un formulario
-      $nuevaPartida = ($vidas == $vidasMax && !isset($_POST["vidas"]));
+      // Una partida es nueva si no se ha recibido alguna de las coordenadas de un disparo
+      $nuevaPartida = (!isset($_POST["coordX"]));
 
 
       $tablero = [];
       $disparosHistorialCadena = "";
   
       
-      // Inicialización del tablero
-      if ($nuevaPartida) {
-        $victoria = false;
-
-        // Creando un tablero con casillas vacías
-        for ($i = 0; $i < $tableroTamano; $i++) {
-          $tableroFila = [];
-          for ($j = 0; $j < $tableroTamano; $j++) {
-            array_push($tableroFila, CASILLA_VACIA);
+      switch ($nuevaPartida) {
+        // Inicialización del tablero
+        case true:
+          if ($nuevaPartida) {
+            $victoria = false;
+    
+            // Creando un tablero con casillas vacías
+            for ($i = 0; $i < $tableroTamano; $i++) {
+              $tableroFila = [];
+              for ($j = 0; $j < $tableroTamano; $j++) {
+                array_push($tableroFila, CASILLA_VACIA);
+              }
+              array_push($tablero, $tableroFila);
+      
+            }
+    
+            // Iterando sobre el arreglo de barcos a crear, y creándolos en coordenadas aleatorias
+            foreach ($barcosLista as $barcoLong) {
+              $tablero = crearBarco($tablero, $barcoLong);
+            }
           }
-          array_push($tablero, $tableroFila);
-  
-        }
+          break;
 
-        // Iterando sobre el arreglo de barcos a crear, y creándolos en coordenadas aleatorias
-        foreach ($barcosLista as $barcoLong) {
-          $tablero = crearBarco($tablero, $barcoLong);
-        }
+        // Cuando la partida ya está avanzada
+        default:
+          $tablero = $_POST["tablero"];
 
-      }
-      else {
-
-        // Fueron enviadas por el formulario varias casillas de nombre 
-        // casilla,X,Y . Donde X y Y son las coordenadas de la casilla
-        // Por lo tanto, podemos iterar por cada una de ellas y reconstruir el 
-        // tablero a partir de ellas
-        for ($x = 0; $x < $tableroTamano; $x++) {
-          $tableroFila = [];
-          for ($y = 0; $y < $tableroTamano; $y++) {
-            $casillaEnviada = "casilla," . $x . "," . $y;
-            $valorCasilla = $_POST[$casillaEnviada];
-
-            // Devolviendo las casillas donde hubo un disparo fallido a una casilla vacía
-            $valorCasilla = ($valorCasilla == CASILLA_FALLO) ? CASILLA_VACIA : $valorCasilla;
-            array_push($tableroFila, $valorCasilla);
-          }
-          array_push($tablero, $tableroFila);
-  
-        }
-
-        // Checando si el disparo recibido es válido, lo es si se ha realizado 
-        // dentro de los confines del tablero y la casilla de destino está
-        // vacía o está llena (tiene un barco)
-        $posX = $_POST["coordX"];
-        $posX = ord(strtoupper($posX)) - ord("A");
-        $posY = $_POST["coordY"] - 1;
-        $disparoValido = ($posX < $tableroTamano && ($tablero[$posX][$posY] == CASILLA_VACIA || $tablero[$posX][$posY] == CASILLA_LLENA));
-
-
-        if ($disparoValido) {
-          switch ($tablero[$posX][$posY]) {
-            case CASILLA_LLENA:
-              $tablero[$posX][$posY] = CASILLA_GOLPE;
-              break;
-            default:
-              $tablero[$posX][$posY] = CASILLA_FALLO;
-              $vidas--;
-              break;
+          // Checando si ya no quedan casillas con barcos en el tablero, de otra manera, no se ha alcanzado la victoria
+          $victoria = true;
+          for ($x = 0; $x < $tableroTamano; $x++) {
+            for ($y = 0; $y < $tableroTamano; $y++) {
+              $tablero[$x][$y] = ($tablero[$x][$y] == CASILLA_FALLO) ? CASILLA_VACIA : $tablero[$x][$y];
+            }
           }
 
-          // Recibiendo el historial de disparos que fue enviado por el formulario,
-          // añadiendo el nuevo disparo al historial
-          if (isset($_POST["historialDisparos"])) {
+          // Checando si el disparo recibido es válido, lo es si se ha realizado 
+          // dentro de los confines del tablero y la casilla de destino está
+          // vacía o está llena (tiene un barco)
+          $posX = $_POST["coordX"];
+          $posX = ord(strtoupper($posX)) - ord("A");
+          $posY = $_POST["coordY"] - 1;
+          $disparoValido = ($posX < $tableroTamano && $tablero[$posX][$posY] != CASILLA_GOLPE);
+          $disparoValido = ($posX >= 0 && $posY >= 0 && $posY < $tableroTamano) ? $disparoValido : false;
 
-            if ($_POST["historialDisparos"] == "") {
-              $disparosHistorialCadena = $_POST["coordX"] . $_POST["coordY"];
+
+          if ($disparoValido) {
+            switch ($tablero[$posX][$posY]) {
+              case CASILLA_LLENA:
+                $tablero[$posX][$posY] = CASILLA_GOLPE;
+                break;
+              default:
+                $tablero[$posX][$posY] = CASILLA_FALLO;
+                $vidas--;
+                break;
+            }
+
+            // Recibiendo el historial de disparos que fue enviado por el formulario.
+            if (isset($_POST["historialDisparos"])) {
+              $disparosHistorialCadena = $_POST["historialDisparos"];
+            }
+            // Añadiendo el nuevo disparo al historial
+            if ($disparosHistorialCadena == "") {
+              $disparosHistorialCadena = strtoupper($_POST["coordX"]) . $_POST["coordY"];
             }
             else {
-              $disparosHistorialCadena = $_POST["historialDisparos"]. ", " . $_POST["coordX"] . $_POST["coordY"];
+              $disparosHistorialCadena .= ", " . strtoupper($_POST["coordX"]) . $_POST["coordY"];
             }  
           }
-        }
 
-        // Cambiando el valor de $victoria si ninguna casilla tiene un barco
-        $victoria = true;
-        for ($i = 0; $i < $tableroTamano; $i++) {
-          for ($j = 0; $j < $tableroTamano; $j++) {
-            if ($tablero[$i][$j] == CASILLA_LLENA) {
-              $victoria = false;
+          // Checando si ya no quedan casillas con barcos en el tablero, de otra manera, no se ha alcanzado la victoria
+          $victoria = true;
+          for ($x = 0; $x < $tableroTamano; $x++) {
+            for ($y = 0; $y < $tableroTamano; $y++) {
+              if ($tablero[$x][$y] == CASILLA_LLENA) {
+                $victoria = false;
+              }
             }
           }
-
-        }
-
+          break;
       }
 
-      // Desplegando información del juego en pantalla
+
+      /* 
+      Desplegando información del juego en pantalla 
+      */
       if ($vidas > 0 && $victoria == false) {
         echo "<strong>Vidas: </strong>";
 
@@ -257,11 +259,9 @@
         // Enviando la información de cada una de las casillas
         for ($i = 0; $i < $tableroTamano; $i++) {
           for ($j = 0; $j < $tableroTamano; $j++) {
-            $casilla = "casilla," . $i . "," . $j;
-            echo "<input type=\"hidden\" name=\"$casilla\" value=\"" . $tablero[$i][$j] . "\">";
-          }    
+            echo "<input type=\"hidden\" name=\"tablero[$i][]\" value=\"" . $tablero[$i][$j] . "\">";
+          } 
         }
-        
         echo "<label> Posición en X: 
                 <input type=\"text\" maxlength=\"1\" maxlenght=\"1\" name=\"coordX\" required>
               </label>";
@@ -274,6 +274,7 @@
       }
 
       // Formulario para elegir dificultad y reiniciar juego.
+      echo "<br><br>";
       echo "<form action=\"index.php\" method=\"POST\">";
       echo "<label>Elige una dificultad para reiniciar el juego
               <select name=\"dificultad\">
@@ -288,6 +289,5 @@
 
     ?>
     <br><br>
-
   </body>
 </html>
